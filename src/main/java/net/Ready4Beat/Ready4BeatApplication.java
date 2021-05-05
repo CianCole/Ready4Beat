0aed5f8b7708cdce69b49dc94536d41392b20c8c
Cian Cole <cian.cole@grandcentrix.net>
2021-05-05 09:15:34 +0200

    "initial commit"

---
+++ src/main/java/net/Ready4Beat/Ready4BeatApplication.java
@@ -0,0 +1,13 @@
+package net.Ready4Beat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ready4BeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ready4BeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/Ready4Beat/Ready4BeatApplicationTests.java
@@ -0,0 +1,13 @@
+package net.Ready4Beat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ready4BeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
